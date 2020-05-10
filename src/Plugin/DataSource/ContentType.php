16011748afbab8cc728543a70598ab16e18c3ab2
Mark Jordan <mjordan@sfu.ca>
2020-05-10 15:25:27 +0000

    Add report on node by content type.

---
+++ src/Plugin/DataSource/ContentType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\islandora_repository_reports\Plugin\DataSource;
+
+use Drupal\islandora_repository_reports\Plugin\DataSource\IslandoraRepositoryReportsDataSourceInterface;
+
+/**
+ * Data source plugin that gets node by Islandra model.
+ */
+class ContentType implements IslandoraRepositoryReportsDataSourceInterface {
+
+  /**
+   * Returns the data source's name.
+   *
+   * @return string
+   *   The name of the data source.
+   */
+  public function getName() {
+    return t('Node Content Type');
+  }
+
+  /**
+   * Returns the data source's chart type.
+   *
+   * @return string
+   *   Either 'pie' or 'bar'.
+   */
+  public function getChartType() {
+    return 'pie';
+  }
+
+  /**
+   * Gets the data.
+   *
+   * @return array
+   *   An assocative array containing formatlabel => count members. 
+   */
+  public function getData() {
+    $entity_type_manager = \Drupal::service('entity_type.manager');
+    $node_storage = $entity_type_manager->getStorage('node');
+    $result = $node_storage->getAggregateQuery()
+      ->groupBy('type')
+      ->aggregate('type', 'COUNT')
+      ->execute();
+    $type_counts = [];
+    foreach ($result as $type) {
+      $type_counts[$type['type']] = $type['type_count'];
+    }
+    return $type_counts;
+  }
+}
