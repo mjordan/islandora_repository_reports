30821b4fc382dc14e934c019014d25a4761e8d1f
Mark Jordan <mjordan@sfu.ca>
2020-05-13 03:13:05 +0000

    Work on #1 - added ability to group disk usage report by filesystem or MIME type.

--- src/Plugin/DataSource/DiskUsage.php
+++ src/Plugin/DataSource/DiskUsage.php
@@ -27,32 +27,55 @@ class DiskUsage implements IslandoraRepositoryReportsDataSourceInterface {
    * {@inheritdoc}
    */
   public function getChartTitle() {
-    return '@total GB total disk usage, grouped by Drupal filesystem.';
+    return '@total GB total disk usage.';
   }
 
   /**
    * {@inheritdoc}
    */
   public function getData() {
+    if ($tempstore = \Drupal::service('user.private_tempstore')->get('islandora_repository_reports')) {
+      if ($form_state = $tempstore->get('islandora_repository_reports_report_form_values')) {
+        $disk_usage_type = $form_state->getValue('islandora_repository_reports_disk_usage_type');
+      }
+    }
+    else {
+      $disk_usage_type = 'filesystem';
+    }
+	  
     $database = \Drupal::database();
-    $result = $database->query("SELECT uri, filesize FROM {file_managed}");
+    $result = $database->query("SELECT uri, filesize, filemime FROM {file_managed}");
 
     $filesystem_usage = [];
-    foreach ($result as $row) {
-      $filesystem = strtok($row->uri, ':');
-      if (array_key_exists($filesystem, $filesystem_usage)) {
-        $filesystem_usage[$filesystem] = $filesystem_usage[$filesystem] + $row->filesize;
+    if ($disk_usage_type == 'filesystem') {
+      foreach ($result as $row) {
+        $filesystem = strtok($row->uri, ':');
+        if (array_key_exists($filesystem, $filesystem_usage)) {
+          $filesystem_usage[$filesystem] = $filesystem_usage[$filesystem] + $row->filesize;
+        }
+        else {
+          $filesystem_usage[$filesystem] = $row->filesize;
+	}
       }
-      else {
-        $filesystem_usage[$filesystem] = $row->filesize;
+    }
+
+    if ($disk_usage_type == 'mimetype') {
+      foreach ($result as $row) {
+        if (array_key_exists($row->filemime, $filesystem_usage)) {
+          $filesystem_usage[$row->filemime] = $filesystem_usage[$row->filemime] + $row->filesize;
+        }
+        else {
+          $filesystem_usage[$row->filemime] = $row->filesize;
+        }
       }
     }
 
     // Drupal gives us bytes, so we convert to GB.
-    foreach ($filesystem_usage as $filesystem => $usage) {
-      $filesystem_usage[$filesystem] = round($usage / 1024 / 1024 / 1024, 3);
+    $converted_filesystem_usage = [];
+    foreach ($filesystem_usage as $key => $usage) {
+      $converted_filesystem_usage[$key] = round($usage / 1024 / 1024 / 1024, 4);
     }
 
-    return $filesystem_usage;
+    return $converted_filesystem_usage;
   }
 }
