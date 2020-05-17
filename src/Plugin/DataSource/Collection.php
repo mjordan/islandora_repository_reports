c9ee215d72759ccfa2cacee36a5520e2075817b1
Mark Jordan <mjordan@sfu.ca>
2020-05-17 21:41:13 +0000

    Added file left out of work on #15.

---
+++ src/Plugin/DataSource/Collection.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\islandora_repository_reports\Plugin\DataSource;
+
+use Drupal\islandora_repository_reports\Plugin\DataSource\IslandoraRepositoryReportsDataSourceInterface;
+
+/**
+ * Data source plugin that gets nodes by Islandora collection.
+ */
+class Collection implements IslandoraRepositoryReportsDataSourceInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return t('Nodes by Islandora collection');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChartType() {
+    return 'pie';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getChartTitle() {
+    return '@total total nodes broken down by collection.';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getData() {
+    $entity_type_manager = \Drupal::service('entity_type.manager');
+    $node_storage = $entity_type_manager->getStorage('node');
+    $result = $node_storage->getAggregateQuery()
+      ->groupBy('field_member_of')
+      ->aggregate('field_member_of', 'COUNT')
+      ->execute();
+    $collection_counts = [];
+    foreach ($result as $collection) {
+      if ($collection_node = \Drupal::entityTypeManager()->getStorage('node')->load($collection['field_member_of_target_id'])) {
+        $collection_counts[$collection_node->getTitle()] = $collection['field_member_of_count'];
+      }
+    }
+    return $collection_counts;
+  }
+}
